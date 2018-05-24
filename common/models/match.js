1aecc9b6f23f6cc71130dff3bc91eb6cede5908b
Konrad <dzieniu@gmail.com>
2018-05-25 00:27:28 +0200

    populate matches, fixed routes

--- common/models/match.js
+++ common/models/match.js
@@ -2,60 +2,77 @@
 
 module.exports = function(Match) {
 
-Match.matches = function(uid,gid,cb) {
-    Match.find({"where":{"group_id": {"like":`${gid.toString()}`}}},(err,matches)=>{
-        let matchesIds = [];
-        matches.forEach(elem=>{
-            matchesIds.push({"matchId":elem.id})
-        })
-        Match.app.models.Bet.find({"where":{"user_id": `${uid}`,"or": matchesIds}},(err,bets)=>{
-            let noMatch = [];
-            bets.forEach(elem=>{
-                noMatch.push(elem.matchId)
-            })
-            Match.find({"where":{"id":{"nin": noMatch}}},(err,matchesr)=>{
-                cb(null,matchesr)
-            })
-        })
-    })
-};
-//TODO dopisac warunek daty
-Match.userMatchesLeft = function(uid,gid,cb) {
-    Match.find({"where":{"group_id": {"like":`${gid.toString()}`}}},(err,matches)=>{
-        let matchesIds = [];
-        matches.forEach(elem=>{
-            matchesIds.push({"matchId":elem.id})
-        })
-        Match.app.models.Bet.find({"where":{"user_id": `${uid}`,"or": matchesIds}},(err,bets)=>{
-            cb(null,bets)
-        })
-    })
-};
-//TODO dopisac warunek daty
-Match.userMatchesDone = function(uid,gid,cb) {
-    Match.find({"where":{"group_id": {"like":`${gid.toString()}`}}},(err,matches)=>{
-        let matchesIds = [];
-        matches.forEach(elem=>{
-            matchesIds.push({"matchId":elem.id})
-        })
-        Match.app.models.Bet.find({"where":{"user_id": `${uid}`,"or": matchesIds}},(err,bets)=>{
-            cb(null,bets)
-        })
-    })
-};
+  Match.matches = function(uid,gid,cb) {
+      Match.find({"where":{"group_id": {"like":`${gid.toString()}`}}},(err,matches)=>{
+          let matchesIds = [];
+          matches.forEach(elem=>{
+              matchesIds.push(elem.id)
+          })
+          console.log(matchesIds)
+          Match.app.models.Bet.find({"where":{"user_id": {"like":`${uid}`},'matchId':{"inq" : matchesIds}}},(err,bets)=>{
+              let noMatch = [];
+              bets.forEach(elem=>{
+                  noMatch.push(elem.matchId)
+              })
+              console.log(noMatch)
+              Match.find({"where":{ "id":{"nin": noMatch}}},(err,matchesr)=>{
+                  cb(null,matchesr)
+              })
+          })
+      })
+  };
 
-Match.remoteMethod(
-    'matches', {
-        http: {
-        path: '/bets/:uid/:gid',
-        verb: 'get'
-        },
-        'accepts': [{ arg: 'uid', type: 'string' },{ arg: 'gid', type: 'string' }],
-        returns: {
-        arg: 'data',
-        type: 'array'
-        }
-    },
+  Match.userMatchesLeft = function(uid,gid,cb) {
+      Match.find({"where":{"group_id": {"like":`${gid.toString()}`}}},(err,matches)=>{
+          let matchesIds = [];
+          matches.forEach(elem=>{
+              matchesIds.push(elem.id)
+          })
+          Match.app.models.Bet.find({"where":{"date":{lt:new Date().yyyymmddhhmm()},"user_id": {"like":`${uid}`},'matchId':{"inq" : matchesIds}}},(err,bets)=>{
+                let n = 0;
+                (function async(){
+                  if(n<=bets.length-1){
+                    Match.findOne({
+                      where:{"id": `${bets[n].matchId}`}
+                    },(err,match)=>{
+                      bets[n].match = match;
+                      n++;
+                      async();
+                    })
+                  }else{
+                    cb(null,bets)
+                  }
+                })()
+          })
+      })
+  };
+
+  Match.userMatchesDone = function(uid,gid,cb) {
+      Match.find({"where":{"group_id": {"like":`${gid.toString()}`}}},(err,matches)=>{
+          let matchesIds = [];
+          matches.forEach(elem=>{
+              matchesIds.push(elem.id)
+          })
+          Match.app.models.Bet.find({"where":{"date":{gt:new Date().yyyymmddhhmm()},"user_id": {"like":`${uid}`},'matchId':{"inq" : matchesIds}},include: 'matches'},(err,bets)=>{
+            let n = 0;
+            (function async(){
+              if(n<=bets.length-1){
+                Match.findOne({
+                  where:{"id": `${bets[n].matchId}`}
+                },(err,match)=>{
+                  bets[n].match = match;
+                  n++;
+                  async();
+                })
+              }else{
+                cb(null,bets)
+              }
+            })()
+          })
+      })
+  };
+
+  Match.remoteMethod(
     'userMatchesLeft', {
         http: {
         path: '/bets/userleft/:uid/:gid',
@@ -63,20 +80,36 @@ Match.remoteMethod(
         },
         'accepts': [{ arg: 'uid', type: 'string' },{ arg: 'gid', type: 'string' }],
         returns: {
-        arg: 'data',
-        type: 'array'
+          type: ['match'],
+          root:true
         }
-    },
-    'userMatchesDone', {
+    }
+  );
+  Match.remoteMethod(
+      'matches', {
         http: {
-        path: '/bets/userdone/:uid/:gid',
+        path: '/bets/:uid/:gid',
         verb: 'get'
         },
         'accepts': [{ arg: 'uid', type: 'string' },{ arg: 'gid', type: 'string' }],
         returns: {
         arg: 'data',
-        type: 'array'
+        type: ['match'],
+        root:true
         }
     }
-);
+  )
+  Match.remoteMethod(
+'userMatchesDone', {
+  http: {
+  path: '/bets/userdone/:uid/:gid',
+  verb: 'get'
+  },
+  'accepts': [{ arg: 'uid', type: 'string' },{ arg: 'gid', type: 'string' }],
+  returns: {
+    type: ['match'],
+    root:true
+  }
+}
+  )
 };
